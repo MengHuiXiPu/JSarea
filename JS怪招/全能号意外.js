83de43d9ee122fcab2450a0dae3ed19830839aa7
Amarantine <cleanqqEmail@163.com>
2021-03-30 09:56:38 +0800

    JS偏方

---
+++ JS怪招/全能号意外.js
@@ -0,0 +1,19 @@
+console.log('常规的情况')
+let a
+console.log(a)
+console.log(a===a-1)
+
+console.log('Infinity的情况')
+let b=Infinity
+console.log(b)
+console.log(b===b-1)
+
+console.log('-Infinity的情况')
+let c=-Infinity
+console.log(c)
+console.log(c===c-1)
+
+console.log('NAN的情况')
+let d=NaN
+console.log(d)
+console.log(d===NaN)
